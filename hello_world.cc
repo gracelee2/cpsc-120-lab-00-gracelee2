66ac89693ac9a9c91e09380e364430431379693c
Grace Lee <grace1@csu.fullerton.edu>
2021-09-09 15:37:21 -0700

    Changed the hello world message to show my name. This is so awesome! I even recompiled it and verified that it works.

--- hello_world.cc
+++ hello_world.cc
@@ -1,12 +1,12 @@
-//Grace Lee
-//CPSC 120-18
-//2021-09-08
-//grace1@csu.fullerton.edu
+// Grace Lee
+// CPSC 120-18
+// 2021-09-08
+// grace1@csu.fullerton.edu
 //@gracelee2
 //
-//Lab 00-00
+// Lab 00-00
 //
-//This is my first program and it prints out Hello Grace!
+// This is my first program and it prints out Hello Grace!
 //
 #include <iostream>
 
