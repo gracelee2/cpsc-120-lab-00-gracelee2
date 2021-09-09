5ab09cc404112423df8cb5fe1b2cdf1e834b6052
Grace Lee <grace1@csu.fullerton.edu>
2021-09-08 17:57:10 -0700

    Changed the hello world message to show my name. This is so awesome! I even recompiled it and verified that it works.

--- hello_world.cc
+++ hello_world.cc
@@ -1,9 +1,18 @@
-
+//Grace Lee
+//CPSC 120-01
+//2021-09-08
+//grace1@csu.fullerton.edu
+//@gracelee2
+//
+//Lab 00-00
+//
+//This is my first program and it prints out Hello Grace!
+//
 #include <iostream>
 
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-  cout << "Hello world!\n";
+  cout << "Hello Grace!\n";
   return 0;
 }
